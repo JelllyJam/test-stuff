5649e80084e715430bd882895972462c084bf2f9
JelllyJam <jc11jm10@gmail.com>
2024-07-14 10:04:46 -0400

    i made a s no s test file

---
+++ sNosTest.py
@@ -0,0 +1,21 @@
+# this file if for processing the data from the S vs No S test
+import numpy as np
+import matplotlib as mtp
+
+#Input data
+#Test 1 (smaller test)
+nos1 = np.array([81, 79])
+s1 = np.array([79, 83])
+#Test 2 (larger test)
+nos2 = np.array([73, 72, 79, 76, 72, 82, 80, 71, 70])
+s2 = np.array([71, 85, 81, 84, 85, 78, 888, 62, 74])
+#1: the average for each category for the smaller, first test and the larger, second test
+nos1_ave = np.mean(nos1)
+s1_ave = np.mean(s1)
+print('no s 1 average: ',nos1_ave)
+print('s1 average: ',s1_ave)
+nos2_ave =
+s2_ave = 
+#2: the graph of the tests over the days, to see trends
+
+print('Done')
