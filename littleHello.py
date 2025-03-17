623577381f89912e5fd03fe54341fbc6c65e9e28
JelllyJam <jc11jm10@gmail.com>
2025-03-17 00:28:56 -0400

    Update littleHello.py

--- littleHello.py
+++ littleHello.py
@@ -1,3 +1,9 @@
 # test file to see what gets sent where
 
 print('hello jom')
+
+def niceHello(name):
+    # this is how you can make a function print out information without a return statement
+    print('hello, %s' % name)
+
+niceHello('jimothy')
